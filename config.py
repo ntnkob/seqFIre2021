ac7188daf822ac5bfaa508a42ad51d50b825d12c
ntnkob <nathanont14@gmail.com>
2021-07-16 12:44:57 +0700

    Add download feature - Add config for file path

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-da-secret-key'
+    OUTPUT_PATH = basedir = os.path.join(os.path.join(os.path.realpath(os.path.dirname(__file__)),'app'),'download')
